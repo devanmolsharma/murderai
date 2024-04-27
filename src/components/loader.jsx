0a73b471796ec5ab7fabca7f4e90ec3416d722b3
Anmol Sharma <keepupsharma@gmail.com>
2024-04-27 18:37:20 -0500

    added code

---
+++ src/components/loader.jsx
@@ -0,0 +1,10 @@
+const Loader = (props) => {
+  return (
+    <div className="wrapper" {...props}>
+      <div className="spinner"></div>
+      <p>{props.children}</p>
+    </div>
+  );
+};
+
+export default Loader;
